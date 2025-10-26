2a08dca14bb47a1e7707ac7abd1de213e2242b50
hndraphyy <hendraaditya1913@gmail.com>
2025-10-26 20:01:44 +0700

    chore: setup routing

---
+++ src/router/Router.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter, Navigate } from 'react-router-dom'; 
+import LoginPage from '../pages/auth/LoginPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="/login" replace />, 
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+]);
+
+export default router;
