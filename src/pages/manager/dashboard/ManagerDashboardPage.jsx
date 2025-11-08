831800b0bc8614869b044b298237b4747ebfcd01
hndraphyy <hendraaditya1913@gmail.com>
2025-11-08 19:36:09 +0700

    add title manager dashboard

--- src/pages/manager/dashboard/ManagerDashboardPage.jsx
+++ src/pages/manager/dashboard/ManagerDashboardPage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import useTitle from "../../hooks/useTitle";
+import usePageTitle from "../../../hooks/usePageTitle";
 
 const ManagerDashboardPage = () => {
+  usePageTitle("Manager Dashboard");
+
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-gray-800">MANAGER DASHBOARD</h1>
