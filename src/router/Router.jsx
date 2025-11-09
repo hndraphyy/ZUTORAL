8247018eb57338a16026a3ba3536aba75c6f3cb3
hndraphyy <hendraaditya1913@gmail.com>
2025-11-09 13:12:28 +0700

    add routing in page manager

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
+
 import LoginPage from "../pages/auth/LoginPage";
 import ManagerLayout from "../layouts/ManagerLayout";
 import SalesAgentLayout from "../layouts/SalesAgentLayout";
+
 import ManagerDashboardPage from "../pages/manager/dashboard/ManagerDashboardPage";
 import ManagerProductsPage from "../pages/manager/products/ManagerProductsPage";
 import ManagerEmployeesPage from "../pages/manager/employees/ManagerEmployeesPage";
+import ManagerReportsPage from "../pages/manager/reports/ManagerReportsPage";
+import ManagerSettingsPage from "../pages/manager/settings/ManagerSettingsPage";
+
 import SalesAgentDashboardPage from "../pages/sales-agent/dashboard/SalesAgentDashboardPage";
 
 const router = createBrowserRouter([
@@ -23,6 +28,8 @@ const router = createBrowserRouter([
       { path: "dashboard", element: <ManagerDashboardPage /> },
       { path: "products", element: <ManagerProductsPage /> },
       { path: "employees", element: <ManagerEmployeesPage /> },
+      { path: "reports", element: <ManagerReportsPage /> },
+      { path: "settings", element: <ManagerSettingsPage /> },
     ],
   },
   {
