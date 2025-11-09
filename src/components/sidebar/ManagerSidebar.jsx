553f9a734132df09a917adad77ffd57f9ba6582c
hndraphyy <hendraaditya1913@gmail.com>
2025-11-09 11:53:12 +0700

    feat: add nav sidebar

--- src/components/sidebar/ManagerSidebar.jsx
+++ src/components/sidebar/ManagerSidebar.jsx
@@ -2,31 +2,53 @@ import { NavLink } from "react-router-dom";
 
 const ManagerSidebar = () => {
   const links = [
-    { to: "/manager/dashboard", label: "Dashboard" },
-    { to: "/manager/reports", label: "Reports" },
+    {
+      to: "/manager/dashboard",
+      label: "Dashboard",
+      icon: "/assets/svg/sidebar-icon/dashboard.svg",
+      iconActive: "/assets/svg/sidebar-icon/active/dashboard.svg",
+    },
+    {
+      to: "/manager/products",
+      label: "Products",
+      icon: "/assets/svg/sidebar-icon/products.svg",
+      iconActive: "/assets/svg/sidebar-icon/active/products.svg",
+    },
+    {
+      to: "/manager/employees",
+      label: "Employees",
+      icon: "/assets/svg/sidebar-icon/employees.svg",
+      iconActive: "/assets/svg/sidebar-icon/active/employees.svg",
+    },
   ];
 
   return (
-    <aside className="w-[318px] bg-sidebar p-4 flex flex-col items-center gap-8 bg-cover">
+    <aside className="w-[318px] bg-sidebar py-10 px-5 flex flex-col items-center gap-8 bg-cover">
       <img
         src="/assets/svg/logo-brand.svg"
         alt="Logo Brand"
         className="w-22 h-auto"
       />
-      <nav className="flex flex-col gap-3">
-        {links.map(({ to, label }) => (
-          <NavLink
-            key={to}
-            to={to}
-            className={({ isActive }) =>
-              `block px-3 py-2 rounded-lg ${
-                isActive
-                  ? "bg-blue-500 text-white"
-                  : "text-gray-700 hover:bg-gray-100"
-              }`
-            }
-          >
-            {label}
+
+      <nav className="flex flex-col gap-3 w-full">
+        {links.map(({ to, label, icon, iconActive }) => (
+          <NavLink key={to} to={to}>
+            {({ isActive }) => (
+              <div
+                className={`flex items-center gap-2.5 px-5 py-3 text-xl rounded-lg font-medium transition-colors ${
+                  isActive
+                    ? "bg-white text-purple"
+                    : "text-white bg-transparent"
+                }`}
+              >
+                <img
+                  src={isActive ? iconActive : icon}
+                  alt={`${label} icon`}
+                  className="w-6 h-6 inline-block"
+                />
+                {label}
+              </div>
+            )}
           </NavLink>
         ))}
       </nav>
